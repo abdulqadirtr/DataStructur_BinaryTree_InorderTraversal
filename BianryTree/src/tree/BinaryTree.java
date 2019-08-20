bb76cde8ef36131ddb3c2808ad69d42fd68d1c13
Abdulqadir <qadirabdul20@yahoo.com>
2019-08-20 11:48:36 +0200

    Binary Tree insertion and InorderTravers

---
+++ BianryTree/src/tree/BinaryTree.java
@@ -0,0 +1,70 @@
+package tree;
+
+public class BinaryTree {
+	
+	Node root;
+	
+	void insert(int key, String name) {
+		/*
+		 * All the Values is inserted to newNode
+		 */
+		Node newNode= new Node(key, name);
+		/*
+		 * Here the root is checking firt time it's null soo we put the values
+		 */
+		if(root==null) {
+			//this will assign the values of newnode key to root intially
+		   root=newNode;
+		}
+		else {
+			/*
+			 * In the Second value the condition will start from here
+			 * We will make focus node and then keep putting values through parent.left to newNode
+			 */
+			Node focusNode=root;
+			Node parent;
+			while(true) {
+				parent=focusNode;
+				if(key<focusNode.key) {
+				focusNode=focusNode.leftChild;
+				if(focusNode==null) {
+					parent.leftChild=newNode;
+					//System.out.println(parent.leftChild);
+					return ;
+				}
+				}
+				else {
+					focusNode=focusNode.rightChild;
+					if(focusNode==null) {
+						parent.rightChild=newNode;
+						
+						return;
+					}
+				}
+			}
+		} 
+	}
+	public void inOrderTraverseTree(Node focusNode) {
+		if(focusNode!=null) {
+			inOrderTraverseTree(focusNode.leftChild);
+			System.out.println(focusNode);
+			inOrderTraverseTree(focusNode.rightChild);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryTree tree=new BinaryTree();
+		tree.insert(50, "boss");
+		tree.insert(25, "vice president");
+		tree.insert(15, "Office Manager");
+		tree.insert(30, "Sectray");
+		tree.insert(85, "Sales Manager");
+		tree.insert(26, "Sales");
+		tree.inOrderTraverseTree(tree.root);
+		
+
+	}
+
+}
